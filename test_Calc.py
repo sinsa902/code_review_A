ff26d35d9ae05cb8466a607250641f8374948447
gyu.byeon <isoul902@gmail.com>
2024-07-10 16:41:56 +0900

    [CREATE] 둘째날 작업물 생성

---
+++ test_Calc.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from Calc import Calc
+
+
+class TestCalc(TestCase):
+    def setUp(self):
+        cal = Calc()
+
+    def test_case1(self):
+        self.assertEqual(1, 1)
