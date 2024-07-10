90a405a58c5d1845c9973a8bb9cf91f6f2ec899f
geony_kkk <blueds05@gmail.com>
2024-07-10 16:57:27 +0900

    [feature] Calc 객체 get Minus 기능 및 TC 개발

--- test_Calc.py
+++ test_Calc.py
@@ -9,3 +9,19 @@ class TestCalc(TestCase):
 
     def test_case1(self):
         self.assertEqual(1, 1)
+
+    def test_minus1(self):
+        calc = Calc()
+        expect = 1
+
+        actual = calc.getMinus(3, 2)
+
+        self.assertEqual(actual, expect)
+
+    def test_minus2(self):
+        calc = Calc()
+        expect = -3
+
+        actual = calc.getMinus(0, 3)
+
+        self.assertEqual(actual, expect)
