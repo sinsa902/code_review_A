35db39f53de232ce0e69fc1a795959098e0b5622
Wontae Jeong <dev.jeong@samsung.com>
2024-07-10 16:54:07 +0900

    [feature] Add getZegop
    Signed-off-by: Wontae Jeong <dev.jeong@samsung.com>

--- Calc.py
+++ Calc.py
@@ -1,2 +1,4 @@
 class Calc:
-    pass
+
+    def getZegop(self, num: int) -> int:
+        return num ** 2
--- test_Calc.py
+++ test_Calc.py
@@ -9,3 +9,16 @@ class TestCalc(TestCase):
 
     def test_case1(self):
         self.assertEqual(1, 1)
+
+    def test_getZegop(self):
+        tests = [
+            {"expected": 1, "num": 1},
+            {"expected": 1, "num": -1},
+            {"expected": 4, "num": 2},
+            {"expected": 16, "num": -4},
+            {"expected": 16, "num": -4},
+            {"expected": 100, "num": 10},
+        ]
+        for test in tests:
+            with self.subTest(f"getZegop({test["num"]})"):
+                self.assertEqual(self.cal.getZegop(test["num"]), test["expected"])
