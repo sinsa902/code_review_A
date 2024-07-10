7c266d4afd68bbe2bb0d17b8a07735e53c7a396f
gyu.byeon <isoul902@gmail.com>
2024-07-10 16:58:48 +0900

    [UPDATE] DIVIDE 추가

--- Calc.py
+++ Calc.py
@@ -1,2 +1,7 @@
 class Calc:
     pass
+
+    def getDivide(self, param, param1):
+        if param1 == 0:
+            return 0
+        return param / param1
--- test_Calc.py
+++ test_Calc.py
@@ -9,3 +9,7 @@ class TestCalc(TestCase):
 
     def test_case1(self):
         self.assertEqual(1, 1)
+
+    def test_case_divide(self):
+        rst = self.cal.getDivide(3, 1)
+        self.assertEqual(3, rst)
