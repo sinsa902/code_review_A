be6b744b3daec6045550b76b14cc50783edaa303
gyu.byeon <isoul902@gmail.com>
2024-07-12 10:41:26 +0900

    [ADD]  Stock 테케 등록

--- TradeStock/test_testcase.py
+++ TradeStock/test_testcase.py
@@ -16,3 +16,18 @@ class TradeTestClass(TestCase):
         self.assertEqual(
             functions, ['예약매수("삼성전자",1000)', '예약매수("lg전자",500)', '예약매도("현대자동차",10)']
         )
+
+    def test_case2_Stock(self):
+        stock = Stock("삼성전자")
+        name = stock.get_name()
+        stock.update_price(5000)
+        stock.update_price(3000)
+        stock.update_price(2000)
+        stock.update_price(1000)
+        stock.update_price(8000)
+        price_list = stock.get_price_list()
+
+
+        self.assertEqual(name, "삼성전자")
+        self.assertEqual(price_list, [5000,3000,2000,1000,8000])
+        )
