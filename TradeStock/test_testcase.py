c9d5915128185ff553acaee9c2b1953136a824bb
donghee.lim <ldh5730@naver.com>
2024-07-12 11:05:04 +0900

    customer test 관련 import 정리

--- TradeStock/test_testcase.py
+++ TradeStock/test_testcase.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from Customer import Cusotomer
+from customer import Customer
+from unittest.mock import Mock
 
 
 class TradeTestClass(TestCase):
@@ -7,12 +8,27 @@ class TradeTestClass(TestCase):
         super().setUp()
         self.customer = Mock(Customer)
         self.customer.get_name.return_value = "jack"
-        self.customer.get_stock_list.return_value = ["삼성전자", "현대자동차", "SK텔레콤"]
-        self.customer.get_function_list.return_value = ['예약매수("삼성전자",1000)', '예약매수("lg전자",500)', '예약매도("현대자동차",10)']
+        self.customer.get_stock_list.return_value = [
+            "삼성전자",
+            "현대자동차",
+            "SK텔레콤",
+        ]
+        self.customer.get_function_list.return_value = [
+            '예약매수("삼성전자",1000)',
+            '예약매수("lg전자",500)',
+            '예약매도("현대자동차",10)',
+        ]
 
     def test_case1_Customer(self):
-        self.assertEqual(customer.get_name(), "jack")
-        self.assertEqual(customer.get_stock_list(), ["삼성전자", "현대자동차", "SK텔레콤"])
+        self.assertEqual(self.customer.get_name(), "jack")
         self.assertEqual(
-            customer.get_function_list(), ['예약매수("삼성전자",1000)', '예약매수("lg전자",500)', '예약매도("현대자동차",10)']
+            self.customer.get_stock_list(), ["삼성전자", "현대자동차", "SK텔레콤"]
+        )
+        self.assertEqual(
+            self.customer.get_function_list(),
+            [
+                '예약매수("삼성전자",1000)',
+                '예약매수("lg전자",500)',
+                '예약매도("현대자동차",10)',
+            ],
         )
